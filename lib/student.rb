a79ef5b170b2d31bce2e71d5bbcc959ba7fd326e
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-15 22:15:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,20 +63,24 @@ class Student
 
 
   def self.students_below_12th_grade
-    
+
   end
 
   def self.count_all_students_in_grade_9
-    
+
   end
 
   def self.first_X_students_in_grade_10
-    
+
   end
 
   def self.first_student_in_grade_10
-    
+
   end
 
-  
+  def self.all_students_in_grade_X
+
+  end
+
+
 end
