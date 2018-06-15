f615fd3d5bcb501e2d197bbcb74312f79ac0bf93
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-15 22:15:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,4 +60,23 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+
+  def self.students_below_12th_grade
+    
+  end
+
+  def self.count_all_students_in_grade_9
+    
+  end
+
+  def self.first_X_students_in_grade_10
+    
+  end
+
+  def self.first_student_in_grade_10
+    
+  end
+
+  
 end
