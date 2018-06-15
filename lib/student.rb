60eca6f397afd798fc283ac5cc409c5cc5308caf
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-15 22:13:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,7 @@ class Student
     sql = <<-SQL
       SELECT *
         FROM students
+        WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
